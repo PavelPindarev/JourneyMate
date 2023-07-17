3a4fd95174d105d42bf667f5c1f677288be0e6cc
PavelPindarev <pavel09.1970@abv.bg>
2023-07-17 20:55:25 +0300

    Initial commit. Add starting dependencies.

---
+++ src/main/java/bg/journey/demo/JourneyMateApplication.java
@@ -0,0 +1,13 @@
+package bg.journey.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/journey/demo/JourneyMateApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.journey.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
