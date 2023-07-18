3588c9ee894e3de647b484d6cfe02e82039afba8
PavelPindarev <pavel09.1970@abv.bg>
2023-07-18 16:34:06 +0300

    Created basic User Entity.

---
+++ src/main/java/bg/journey/demo/model/entity/UserEntity.java
@@ -0,0 +1,33 @@
+package bg.journey.demo.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class UserEntity extends BaseEntity{
+    private String username;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String password;
+
+    @Builder.Default
+    private boolean enabled = true;
+    @Builder.Default
+    private boolean locked = false;
+    @Builder.Default
+    private boolean accountExpired = false;
+    @Builder.Default
+    private boolean credentialsExpired = false;
+}
